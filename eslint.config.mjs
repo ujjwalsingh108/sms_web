af9a8cbf0b7ccfaba5d38e66eb067349d622922c
ujjwal-ampcome <ujjwal.singh@ampcome.com>
2025-11-20 23:15:17 +0530

    add eslint disable any errors and warnings globally.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,6 +20,15 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: false,
+    },
+  },
 ];
 
 export default eslintConfig;
